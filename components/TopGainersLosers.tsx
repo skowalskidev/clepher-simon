fae81e06b1487e57e3794a6b792450cfef5d4637
skowalskidev <skowalskidev@gmail.com>
2024-04-01 14:53:28 +0700

    fix rows and CLA

--- components/TopGainersLosers.tsx
+++ components/TopGainersLosers.tsx
@@ -6,7 +6,7 @@ export const TopGainersLosers = ({ topGainersLosersData }: { topGainersLosersDat
     return (
         <>
             {!topGainersLosersData
-                ? <TopGainersLosersSkeleton rows={7} />
+                ? <TopGainersLosersSkeleton rows={8} />
                 : <div className="relative overflow-x-auto p-2 shrink-0">
                     <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 rounded overflow-hidden">
                         <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">

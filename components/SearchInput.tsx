4db69c2a821bc05c6d6025a3d76872293e67245b
skowalskidev <skowalskidev@gmail.com>
2024-04-01 17:56:56 +0700

    fix input zoom on mobile

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -28,7 +28,7 @@ export const SeachInput = ({ onSubmit, onInput, onBlur }: SearchInputParams) => 
                 <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
                     <AiOutlineStock className="text-base text-gray-500 dark:text-gray-400" />
                 </div>
-                <input value={input} onChange={(event) => setInput(event?.target.value)} onBlur={onBlur} type="text" id="simple-search" className="focus:outline-none bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Symbol search..." required />
+                <input value={input} onChange={(event) => setInput(event?.target.value)} onBlur={onBlur} type="text" id="simple-search" className="focus:outline-none bg-gray-50 border border-gray-300 text-gray-900 text-base rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Symbol search..." required />
                 <SearchResults />
             </div>
             <button type="submit" className="p-2.5 ms-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
